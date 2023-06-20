ce0f4d389e8dacc34ba952c3f96ad0f78d95d726
wael Abosamra <tr.wa2el@gmail.com>
2023-06-20 15:27:06 -0700

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,34 @@
+/*
+ * File: 102-fibonacci.c
+ * Auth: Wael AboSamra
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints first 50 Fibonacci numbers, starting with 1 and 2,
+ *        separated by a comma followed by a space.
+ *
+ * Return: 0.
+ */
+int main(void)
+{
+	int i;
+	unsigned long fib1 = 0, fib2 = 1, sum;
+
+	for (i = 0; i < 50; i++)
+	{
+		sum = fib1 + fib2;
+		printf("%lu", sum);
+
+		fib1 = fib2;
+		fib2 = sum;
+
+		if (i == 49)
+			printf("\n");
+		else
+			printf(", ");
+	}
+
+	return (0);
+}
