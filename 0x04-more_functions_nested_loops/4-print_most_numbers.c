768177f6c258e195e034fa1117aed97ed32c5416
wael Abosamra <tr.wa2el@gmail.com>
2023-06-22 14:55:12 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -13,8 +13,8 @@ void print_most_numbers(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		if (i != 2 || i != 4)
-			_putchar((i % 10) + '0')
+		if (i != 2 && i != 4)
+			_putchar((i % 10) + '0');
 	}
-	_putchar('\n')
+	_putchar('\n');
 }
