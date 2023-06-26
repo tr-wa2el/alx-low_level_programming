3761b735c7ee9ba05f84823086f22ffea0e025a6
wael Abosamra <tr.wa2el@gmail.com>
2023-06-26 16:37:32 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - print first char of sets each set consists of tow char
+ * @str: The string.
+ *
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+	
+	while (str[i])
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
