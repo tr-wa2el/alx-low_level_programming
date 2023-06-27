16f877f3671f16614df66dccb2d4860d40948332
wael Abosamra <tr.wa2el@gmail.com>
2023-06-26 17:15:46 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -16,12 +16,14 @@ void puts_half(char *str)
 
 	while (str[i])
 		i++;
+
 	if (i % 2 == 0)
 		l = i / 2;
 	else
-		l = (i - 1) / 2;
+		l = (i + 1) / 2;
 
 	for (index = l; index < i; index++)
 		_putchar(str[index]);
+
 	_putchar('\n');
 }
