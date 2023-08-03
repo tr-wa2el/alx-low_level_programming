ce69d491a9280235ad9206fa45b35a0e97b282aa
wael Abosamra <tr.wa2el@gmail.com>
2023-08-03 15:45:45 -0700

    done

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,23 +6,8 @@
  */
 void print_binary(unsigned long int n)
 {
-	int i, j = 0;
-	unsigned long int temp, mask = 0;
+	if (n > 1)
+		print_binary(n >> 1);
 
-	if (n == 0)
-		_putchar('0');
-	temp = n;
-
-	for (j = 0; temp != 0; j++)
-		temp = temp >> 1;
-
-	for (i = j - 1; i >= 0; i--)
-	{
-		mask = 1 << i;
-		if ((n & mask) != 0)
-			_putchar('1');
-		else
-			_putchar('0');
-	}
+	_putchar((n & 1) + '0');
 }
-
